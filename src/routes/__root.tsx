958ca25bb5d4804c1db70257340a46c9249a6192
Josi <josephinemundt1297@gmail.com>
2026-08-12 14:00:08 +0200

    feat: connect root route to app shell

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,15 +1,14 @@
-import * as React from 'react'
-import { Outlet, createRootRoute } from '@tanstack/react-router'
+import { Outlet, createRootRoute } from '@tanstack/react-router';
+import { AppShell } from '../app/appShell';
 
 export const Route = createRootRoute({
   component: RootComponent,
-})
+});
 
 function RootComponent() {
   return (
-    <React.Fragment>
-      <div>Hello "__root"!</div>
+    <AppShell>
       <Outlet />
-    </React.Fragment>
-  )
+    </AppShell>
+  );
 }
